980eac9e17733b244869107ad7d7b02f92462354
Justin Tormey <jrtormey@gmail.com>
2016-04-23 15:46:36 -0400

    update demo to v1.1.0

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 
   function DemoController($scope, $theme) {
     $scope.theme = $theme.activeTheme;
-    $scope.themes = $theme.themes.map(function(t){return t.name;});
+    $scope.themes = $theme.themes;
     $scope.$watch('theme', $theme.setTheme);
   }
 
